41df4544f392bc73f33644b9df8f209cd7773cf0
Bill Hood <william.arthur.hood@gmail.com>
2023-06-10 21:54:27 -0700

    Corrected copyright date

--- ChangeScan/src/ShowUsage.kt
+++ ChangeScan/src/ShowUsage.kt
@@ -54,7 +54,7 @@ const val LICENSE_PARAGRAPH_1 = "Permission is hereby granted, free of charge, t
 const val LICENSE_PARAGRAPH_2 = "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software."
 const val LICENSE_PARAGRAPH_3 = "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
 fun showLicense() {
-    System.out.println("Change Scan -  $COPYRIGHT 2020 William Arthur Hood")
+    System.out.println("Change Scan -  $COPYRIGHT 2020, 2023 William Arthur Hood")
     System.out.println()
     System.out.println("MIT LICENSE")
     System.out.println()
